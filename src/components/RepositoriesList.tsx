b525b676062ed377dd802aed4212e886550285bb
AustynYou <austyn.you@gmail.com>
2022-12-26 23:52:39 -0800

    ✍️ useActions, bindActionCreators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,22 +1,15 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
 
-  const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // dispatch는 search함수의 인자로 들어가야 될 거 같은데, 오히려 dispatch가 search를 감싸고 있어서 혼란스러움
-    // 이유는 지금 찾아보려 하지말자..
-    dispatch(actionCreators.searchRepositories(term) as any); 
-    console.log(dispatch(actionCreators.searchRepositories(term) as any));
     
-    // 왜 . 을 찍어서 객체?처럼 연결? 하는 게 가능하냐?
-    // 추측인데, export default searchRepositories; 로 한 게 아니라,
-    // 그냥 export searchRepositories; 로 해서 그런 듯.
+    searchRepositories(term);
   }
   return (
     <div>
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,7 +6,7 @@ import { Action } from '../actions';
 // action creator = 액션 생성함수는, 액션을 만드는 함수입니다. 단순히 파라미터를 받아와서 액션 객체 형태로 만들어주죠.
 // 이 함수는 단지 action 객체를 return 해주는 일만 한다.
 export const searchRepositories = (term: string) => {
-  return async (dispatch: Dispatch<Action>) => {
+  return async (dispatch: Dispatch<Action>) => { // 이 dispatch는 도대체 어디서 받아오는 거야?
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     })
